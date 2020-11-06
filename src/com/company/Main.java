758c7c3be928b3beabf90f3acbb9a03bf8021a8f
student <haley.slaney@gmail.com>
2020-11-06 13:36:21 -0500

    commit initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        conditional c1 = new conditional();
+        System.out.println(c1.isLeapYear(2100));
+    }
+}
---
+++ src/com/company/conditional.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class conditional {
+
+    public boolean isLeapYear(int year)
+    {
+        boolean four = year%400 ==0;
+        boolean hundred = year%100 ==0;
+        if (year%4==0 ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+
+
+//    public boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2)
+//    {
+//
+//
+//    }
+}
