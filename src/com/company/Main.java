328904489b2785e0263a83a59ef681a9780f2961
hslaney <haley.slaney@gmail.com>
2020-11-07 16:50:41 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,20 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         conditional c1 = new conditional();
-        System.out.println(c1.isLeapYear(2000));
+//        System.out.println("2000 " + c1.isLeapYear(2000));
+//        System.out.println("2004 " + c1.isLeapYear(2004));
+//        System.out.println("2003 " + c1.isLeapYear(2003));
+//        System.out.println("2100 " + c1.isLeapYear(2100));
+
+
+        // is later
+        System.out.println("1/2/2010 is later than 1/2/2011 " + c1.isLater(1,2,2010, 1,2,2011));
+        System.out.println("1/2/2011 is later than 1/2/2010 " + c1.isLater(1,2,2011, 1,2,2010));
+        System.out.println("1/5/2010 is later than 3/2/2010 " + c1.isLater(1,5,2010, 3,2,2010));
+        System.out.println("3/2/2010 is later than 1/2/2010 " + c1.isLater(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is later than 1/2/2010 " + c1.isLater(1,3,2010, 1,2,2010));
+        System.out.println("1/2/2010 is later than 1/3/2010 " + c1.isLater(1,2,2010, 1,3,2010));
+        System.out.println("1/2/2010 is later than 1/2/2010 " + c1.isLater(1,2,2010, 1,2,2010));
+        System.out.println("4/8/2010 is later than 1/2/2011 " + c1.isLater(4,8,2010, 1,2,2011));
     }
 }
--- src/com/company/conditional.java
+++ src/com/company/conditional.java
@@ -2,12 +2,38 @@ package com.company;
 
 public class conditional {
 
-    public boolean isLeapYear(int year)
-    {
-        boolean four = year%400 ==0;
-        boolean hundred = year%100 ==0;
-        if (year%4==0 && (four && hundred)){
+    public boolean isLeapYear(int year) {
+        boolean years = year % 4 == 0;
+        boolean four = year % 400 == 0;
+        boolean hundred = year % 100 == 0;
+        if (!years) {
+            return false;
+        } else if (!four && hundred) {
+            return false;
+        } else {
             return true;
+        }
+    }
+
+    public boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2) {
+        boolean checkMonth = month2 < month1;
+        boolean checkYear = year2 < year1;
+        boolean checkDay = day2 < day1;
+
+        if (checkYear) {
+            return true;
+        }
+        else if (year2==year1){
+            if (checkMonth){
+                return true;
+
+            }
+            else if (month2==month1){
+                return checkDay;
+            }
+            else {
+                return false;
+            }
         }
         else {
             return false;
@@ -15,12 +41,4 @@ public class conditional {
     }
 
 
-
-
-
-//    public boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2)
-//    {
-//
-//
-//    }
 }
