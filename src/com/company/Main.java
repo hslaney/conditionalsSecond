0551701f35c659ac408de9fe04360615dc5beb2e
hslaney <haley.slaney@gmail.com>
2020-11-10 19:30:11 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,43 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         conditional c1 = new conditional();
-        //isLeapYear
-//        System.out.println("2000 " + c1.isLeapYear(2000));
-//        System.out.println("2004 " + c1.isLeapYear(2004));
-//        System.out.println("2003 " + c1.isLeapYear(2003));
-//        System.out.println("2100 " + c1.isLeapYear(2100));
 
+        //isLeapYear
+        System.out.println("2000 " + c1.isLeapYear(2000));
+        System.out.println("2004 " + c1.isLeapYear(2004));
+        System.out.println("2003 " + c1.isLeapYear(2003));
+        System.out.println("2100 " + c1.isLeapYear(2100));
+        System.out.println();
 
         // is later
-//        System.out.println("1/2/2010 is later than 1/2/2011 " + c1.isLater(1,2,2010, 1,2,2011));
-//        System.out.println("1/2/2011 is later than 1/2/2010 " + c1.isLater(1,2,2011, 1,2,2010));
-//        System.out.println("1/5/2010 is later than 3/2/2010 " + c1.isLater(1,5,2010, 3,2,2010));
-//        System.out.println("3/2/2010 is later than 1/2/2010 " + c1.isLater(3,2,2010, 1,2,2010));
-//        System.out.println("1/3/2010 is later than 1/2/2010 " + c1.isLater(1,3,2010, 1,2,2010));
-//        System.out.println("1/2/2010 is later than 1/3/2010 " + c1.isLater(1,2,2010, 1,3,2010));
-//        System.out.println("1/2/2010 is later than 1/2/2010 " + c1.isLater(1,2,2010, 1,2,2010));
-//        System.out.println("4/8/2010 is later than 1/2/2011 " + c1.isLater(4,8,2010, 1,2,2011));
+        System.out.println("1/2/2010 is later than 1/2/2011 " + c1.isLater(1,2,2010, 1,2,2011));
+        System.out.println("1/2/2011 is later than 1/2/2010 " + c1.isLater(1,2,2011, 1,2,2010));
+        System.out.println("1/5/2010 is later than 3/2/2010 " + c1.isLater(1,5,2010, 3,2,2010));
+        System.out.println("3/2/2010 is later than 1/2/2010 " + c1.isLater(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is later than 1/2/2010 " + c1.isLater(1,3,2010, 1,2,2010));
+        System.out.println("1/2/2010 is later than 1/3/2010 " + c1.isLater(1,2,2010, 1,3,2010));
+        System.out.println("1/2/2010 is later than 1/2/2010 " + c1.isLater(1,2,2010, 1,2,2010));
+        System.out.println("4/8/2010 is later than 1/2/2011 " + c1.isLater(4,8,2010, 1,2,2011));
+        System.out.println();
 
         //bestMatch
-//        System.out.println("Best match 2 3 4 " + c1.bestMatch(2,3,4));
-//        System.out.println("Best match 4 3 3 " + c1.bestMatch(4,3,3));
-//        System.out.println("Best match 3 8 4 " + c1.bestMatch(3,8,4));
-//        System.out.println("Best match 4 3 4 " + c1.bestMatch(4,3,4));
-//        System.out.println("Best match 2 4 4 " + c1.bestMatch(2,4,4));
-//        System.out.println("Best match 8 8 4 " + c1.bestMatch(8,8,4));
-//        System.out.println("Best match 4 4 8 " + c1.bestMatch(4,4,8));
+        System.out.println("Best match 2 3 4 " + c1.bestMatch(2,3,4));
+        System.out.println("Best match 4 3 3 " + c1.bestMatch(4,3,3));
+        System.out.println("Best match 3 8 4 " + c1.bestMatch(3,8,4));
+        System.out.println("Best match 4 3 4 " + c1.bestMatch(4,3,4));
+        System.out.println("Best match 2 4 4 " + c1.bestMatch(2,4,4));
+        System.out.println("Best match 8 8 4 " + c1.bestMatch(8,8,4));
+        System.out.println("Best match 4 4 8 " + c1.bestMatch(4,4,8));
+        System.out.println();
 
         //findBestFit
-//        System.out.println("Find Best fit 2 3 6 is " + c1.findBestFit(2,3,6));
-//        System.out.println("Find Best fit 4 3 6 is " + c1.findBestFit(4,3,6));
-//        System.out.println("Find Best fit 3 4 6 is " + c1.findBestFit(3,4,6));
-//        System.out.println("Find Best fit 2 3 1 is " + c1.findBestFit(2,3,1));
-//        System.out.println("Find Best fit 6 3 4 is " + c1.findBestFit(6,3,4));
-//        System.out.println("Find Best fit 3 6 4 is " + c1.findBestFit(3,6,4));
-
+        System.out.println("Find Best fit 2 3 6 is " + c1.findBestFit(2,3,6));
+        System.out.println("Find Best fit 4 3 6 is " + c1.findBestFit(4,3,6));
+        System.out.println("Find Best fit 3 4 6 is " + c1.findBestFit(3,4,6));
+        System.out.println("Find Best fit 2 3 1 is " + c1.findBestFit(2,3,1));
+        System.out.println("Find Best fit 6 3 4 is " + c1.findBestFit(6,3,4));
+        System.out.println("Find Best fit 3 6 4 is " + c1.findBestFit(3,6,4));
+        System.out.println();
 
         //Make Bench
         System.out.println("Make bench 3 1 8 is " + c1.makeBench(3,1,8));
@@ -49,7 +52,8 @@ public class Main {
         System.out.println("Make bench 3 4 20 is " + c1.makeBench(3,4,20));
         System.out.println("Make bench 0 6 30 is " + c1.makeBench(0,6,30));
         System.out.println("Make bench 0 5 30 is " + c1.makeBench(0,5,30));
-
+        System.out.println("Make bench 3 5 24 is " + c1.makeBench(3,5,24));
+        System.out.println("Make bench 2 6 23 is " + c1.makeBench(2,6,23));
 
     }
 }
--- src/com/company/conditional.java
+++ src/com/company/conditional.java
@@ -6,28 +6,35 @@ public class conditional {
         boolean years = year % 4 == 0;
         boolean fourH = year % 400 == 0;
         boolean hundred = year % 100 == 0;
-        if (!years) {
+        if (!years)
+        {
             return false;
-        } else if (!fourH && hundred) {
+        }
+        else if (!fourH && hundred)
+        {
             return false;
         } else {
             return true;
         }
     }
 
+
     public boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2) {
         boolean checkMonth = month2 < month1;
         boolean checkYear = year2 < year1;
         boolean checkDay = day2 < day1;
 
-        if (checkYear) {
+        if (checkYear)
+        {
             return true;
         }
-        else if (year2==year1){
-            if (checkMonth){
+        else if (year2 == year1)
+        {
+            if (checkMonth)
+            {
                 return true;
             }
-            else if (month2==month1){
+            else if (month2 == month1) {
                 return checkDay;
             }
             else {
@@ -40,71 +47,79 @@ public class conditional {
     }
 
 
-    public String bestMatch(int r, int g, int b){
-        boolean red = r>=g && r>=b;
-        boolean green = g>=r && g>=b;
+    public String bestMatch(int r, int g, int b) {
+        boolean red = r >= g && r >= b;
+        boolean green = g >= r && g >= b;
 
-        if (red){
-            if (r>g && r>b){
+        if (red) {
+            if (r > g && r > b) {
                 return "RED";
-            }
-            else if (r==g && r==b){
+            } else if (r == g && r == b) {
                 return "GRAY";
-            }
-            else if (r==g){
+            } else if (r == g) {
                 return "YELLOW";
-            }
-            else {
+            } else {
                 return "MAGENTA";
             }
-        }
-        else if (green) {
+        } else if (green) {
             if (g > b) {
                 return "GREEN";
-            }
-            else {
+            } else {
                 return "CYAN";
             }
-        }
-        else {
+        } else {
             return "BLUE";
         }
     }
 
-    public int findBestFit(int size1, int size2, int space){
-        boolean both = size1 + size2 <= space;
-        boolean first = size1 + size2 >= space;
-        int x = 0;
+    public int findBestFit(int size1, int size2, int space) {
+        boolean first = size1 + size2 > space;
+        int x;
 
-        if (both){
-            x=3;
-        }
-        else if (first){
-            if (size1>size2 && size1<space || size1==size2 && size1<space || size1<space && size2>space){
+        if (first) {
+            if (size1 > size2 && size1 < space || size1 == size2 && size1 < space || size1 < space && size2 > space) {
                 x = 1;
-            }
-            else if (size2>size1 && size2<space || size2<space && size1>space){
+            } else if (size2 > size1 && size2 < space || size2 < space && size1 > space) {
                 x = 2;
-            }
-            else {
+            } else {
                 x = 0;
             }
+        }
+        else{
+            x=3;
         }
         return x;
     }
 
 
-    public boolean makeBench(int small, int big, int goal){
+    public boolean makeBench(int small, int big, int goal) {
         big *= 5;
         int total = small + big;
 
-        if(total>=goal){
-            return true;
-        }
-        else{
+        if (total >= goal) {
+            if (big == goal || small >= goal || goal == total)
+            {
+                return true;
+            }
+            else
+                {
+                    int y = goal-big;
+                    if (small>=y && y>0)
+                    {
+                        return true;
+                    }
+                    else if (y<0)
+                    {
+                        int z = goal%5;
+                        return small>=z;
+                    }
+                    else {
+                        return false;
+                    }
+                 }
+            }
+        else {
             return false;
         }
     }
-
-
 }
