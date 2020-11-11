c2783a85a42245b698515169121547d1fd4a63cc
hslaney <haley.slaney@gmail.com>
2020-11-10 19:33:22 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -57,3 +57,48 @@ public class Main {
 
     }
 }
+
+
+/*
+
+2000 true
+2004 true
+2003 false
+2100 false
+
+1/2/2010 is later than 1/2/2011 false
+1/2/2011 is later than 1/2/2010 true
+1/5/2010 is later than 3/2/2010 false
+3/2/2010 is later than 1/2/2010 true
+1/3/2010 is later than 1/2/2010 true
+1/2/2010 is later than 1/3/2010 false
+1/2/2010 is later than 1/2/2010 false
+4/8/2010 is later than 1/2/2011 false
+
+Best match 2 3 4 BLUE
+Best match 4 3 3 RED
+Best match 3 8 4 GREEN
+Best match 4 3 4 MAGENTA
+Best match 2 4 4 CYAN
+Best match 8 8 4 YELLOW
+Best match 4 4 8 BLUE
+
+Find Best fit 2 3 6 is 3
+Find Best fit 4 3 6 is 1
+Find Best fit 3 4 6 is 2
+Find Best fit 2 3 1 is 0
+Find Best fit 6 3 4 is 2
+Find Best fit 3 6 4 is 1
+
+Make bench 3 1 8 is true
+Make bench 3 1 9 is false
+Make bench 11 1 15 is true
+Make bench 4 2 15 is false
+Make bench 20 0 20 is true
+Make bench 3 4 20 is true
+Make bench 0 6 30 is true
+Make bench 0 5 30 is false
+Make bench 3 5 24 is false
+Make bench 2 6 23 is false
+
+ */
